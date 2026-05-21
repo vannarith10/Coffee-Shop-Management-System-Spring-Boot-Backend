31d907333eea1aa9d65c38dc5efa2cd6d0746b33
vannarith10 <vyra.vannarith27@gamil.com>
2026-05-22 04:23:04 +0700

    Refactoring Folder Structure - Done OrderQueryServiceImpl

--- src/main/java/com/coffeeshop/api/service/OrderQueryService.java
+++ src/main/java/com/coffeeshop/api/service/OrderQueryService.java
@@ -1,4 +1,11 @@
 package com.coffeeshop.api.service;
 
+import com.coffeeshop.api.dto.order.OrderMessageToBarista;
+
+import java.util.List;
+
 public interface OrderQueryService {
+
+    List<OrderMessageToBarista> baristaGetsOrders ();
+
 }
---
+++ src/main/java/com/coffeeshop/api/service/impl/OrderQueryServiceImpl.java
@@ -0,0 +1,43 @@
+package com.coffeeshop.api.service.impl;
+
+import com.coffeeshop.api.domain.Order;
+import com.coffeeshop.api.domain.enums.OrderStatus;
+import com.coffeeshop.api.dto.order.OrderMessageToBarista;
+import com.coffeeshop.api.mapper.OrderMapper;
+import com.coffeeshop.api.minio.ImageStorageService;
+import com.coffeeshop.api.repository.OrderRepository;
+import com.coffeeshop.api.service.OrderQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.EnumSet;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderQueryServiceImpl implements OrderQueryService {
+
+
+    private final OrderRepository orderRepository;
+    private final OrderMapper orderMapper;
+    private static final Instant CAMBODIA_TIME_NOW = ZonedDateTime.now(ZoneId.of("Asia/Phnom_Penh")).toInstant();
+
+
+    @Override
+    public List<OrderMessageToBarista> baristaGetsOrders() {
+        Instant cutoff = CAMBODIA_TIME_NOW.minus(7, ChronoUnit.DAYS);
+        var statuses = EnumSet.of(OrderStatus.QUEUED, OrderStatus.PREPARING);
+
+        List<Order> orders = orderRepository.findVisibleOrders(statuses, cutoff);
+
+        return orders.stream()
+                .map(orderMapper::toOrderMessageResponseDto)
+                .toList();
+    }
+
+
+}
